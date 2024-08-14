d8572e987226b5f4106cf3f8538d3c829c95a830
raoshun <s.hirao.767@outlook.jp>
2024-08-15 06:56:21 +0900

    update: show subtotal values

--- my-table-app/src/components/Charts.js
+++ my-table-app/src/components/Charts.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
 
-const Charts = ({ positiveChartData, negativeChartData, options }) => {
+const Charts = ({ positiveChartData, negativeChartData, positiveTotal, negativeTotal, options }) => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
             <div style={{ flex: '1 1 45%', maxWidth: '45%', margin: '10px' }}>
-                <h2>収入</h2>
+                <h2>収入: ¥{positiveTotal.toLocaleString()}</h2>
                 <Pie data={positiveChartData} options={options} />
             </div>
             <div style={{ flex: '1 1 45%', maxWidth: '45%', margin: '10px' }}>
-                <h2>支出</h2>
+                <h2>支出: ¥{negativeTotal.toLocaleString()}</h2>
                 <Pie data={negativeChartData} options={options} />
             </div>
         </div>
     );
-}
+};
 
 export default Charts;
